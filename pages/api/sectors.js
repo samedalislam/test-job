5eac63bccb37bde1e7762e804a5b3dce876d2cee
SAMED AL ISLAM <samedalislam>
2023-04-29 08:19:59 +0600

    Modify: Make api for sectors. It will prevent hard coding for sectors in form

---
+++ pages/api/sectors.js
@@ -0,0 +1,320 @@
+export default function sectors(req, res) {
+    res.status(200).json([
+        {
+            value: "1",
+            sector: "Manufacturing",
+        },
+        {
+            value: "19",
+            sector: "Construction materials",
+        },
+        {
+            value: "18",
+            sector: "Electronics and Optics",
+        },
+        {
+            value: "6",
+            sector: "Food and Beverage",
+        },
+        {
+            value: "342",
+            sector: "Bakery & confectionery products",
+        },
+        {
+            value: "43",
+            sector: "Beverages",
+        },
+        {
+            value: "42",
+            sector: "Fish & fish products",
+        },
+        {
+            value: "40",
+            sector: "Meat & meat products",
+        },
+        {
+            value: "39",
+            sector: "Milk & dairy products",
+        },
+        {
+            value: "437",
+            sector: "Other",
+        },
+        {
+            value: "378",
+            sector: "Sweets & snack food",
+        },
+        {
+            value: "13",
+            sector: "Furniture",
+        },
+        {
+            value: "389",
+            sector: "Bathroom/sauna",
+        },
+        {
+            value: "385",
+            sector: "Bedroom",
+        },
+        {
+            value: "390",
+            sector: "Children’s room",
+        },
+        {
+            value: "98",
+            sector: "Kitchen",
+        },
+        {
+            value: "101",
+            sector: "Living room",
+        },
+        {
+            value: "392",
+            sector: "Office",
+        },
+        {
+            value: "394",
+            sector: "Other (Furniture)",
+        },
+        {
+            value: "341",
+            sector: "Outdoor",
+        },
+        {
+            value: "99",
+            sector: "Project furniture",
+        },
+        {
+            value: "12",
+            sector: "Machinery",
+        },
+        {
+            value: "94",
+            sector: "Machinery components",
+        },
+        {
+            value: "91",
+            sector: "Machinery equipment/tools",
+        },
+        {
+            value: "224",
+            sector: "Manufacture of machinery",
+        },
+        {
+            value: "97",
+            sector: "Maritime",
+        },
+        {
+            value: "271",
+            sector: "Aluminium and steel workboats",
+        },
+        {
+            value: "269",
+            sector: "Boat/Yacht building",
+        },
+        {
+            value: "230",
+            sector: "Ship repair and conversion",
+        },
+        {
+            value: "93",
+            sector: "Metal structures",
+        },
+        {
+            value: "508",
+            sector: "Other",
+        },
+        {
+            value: "227",
+            sector: "Repair and maintenance service",
+        },
+        {
+            value: "11",
+            sector: "Metalworking",
+        },
+        {
+            value: "67",
+            sector: "Construction of metal structures",
+        },
+        {
+            value: "263",
+            sector: "Houses and buildings",
+        },
+        {
+            value: "267",
+            sector: "Metal products",
+        },
+        {
+            value: "542",
+            sector: "Metal works",
+        },
+        {
+            value: "75",
+            sector: "CNC-machining",
+        },
+        {
+            value: "62",
+            sector: "Forgings, Fasteners",
+        },
+        {
+            value: "69",
+            sector: "Gas, Plasma, Laser cutting",
+        },
+        {
+            value: "66",
+            sector: "MIG, TIG, Aluminum welding",
+        },
+        {
+            value: "9",
+            sector: "Plastic and Rubber",
+        },
+        {
+            value: "54",
+            sector: "Packaging",
+        },
+        {
+            value: "556",
+            sector: "Plastic goods",
+        },
+        {
+            value: "559",
+            sector: "Plastic processing technology",
+        },
+        {
+            value: "55",
+            sector: "Blowing",
+        },
+        {
+            value: "57",
+            sector: "Moulding",
+        },
+        {
+            value: "53",
+            sector: "Plastics welding and processing",
+        },
+        {
+            value: "560",
+            sector: "Plastic profiles",
+        },
+        {
+            value: "5",
+            sector: "Printing",
+        },
+        {
+            value: "148",
+            sector: "Advertising",
+        },
+        {
+            value: "150",
+            sector: "Book/Periodicals printing",
+        },
+        {
+            value: "145",
+            sector: "Labelling and packaging printing",
+        },
+        {
+            value: "7",
+            sector: "Textile and Clothing",
+        },
+        {
+            value: "44",
+            sector: "Clothing",
+        },
+        {
+            value: "45",
+            sector: "Textile",
+        },
+        {
+            value: "8",
+            sector: "Wood",
+        },
+        {
+            value: "337",
+            sector: "Other (Wood)",
+        },
+        {
+            value: "51",
+            sector: "Wooden building materials",
+        },
+        {
+            value: "47",
+            sector: "Wooden houses",
+        },
+        {
+            value: "3",
+            sector: "Other",
+        },
+        {
+            value: "37",
+            sector: "Creative industries",
+        },
+        {
+            value: "29",
+            sector: "Energy technology",
+        },
+        {
+            value: "33",
+            sector: "Environment",
+        },
+        {
+            value: "2",
+            sector: "Service",
+        },
+        {
+            value: "25",
+            sector: "Business services",
+        },
+        {
+            value: "35",
+            sector: "Engineering",
+        },
+        {
+            value: "28",
+            sector: "Information Technology and Telecommunications",
+        },
+        {
+            value: "581",
+            sector: "Data processing, Web portals, E-marketing",
+        },
+        {
+            value: "576",
+            sector: "Programming, Consultancy",
+        },
+        {
+            value: "121",
+            sector: "Software, Hardware",
+        },
+        {
+            value: "122",
+            sector: "Telecommunications",
+        },
+        {
+            value: "22",
+            sector: "Tourism",
+        },
+        {
+            value: "141",
+            sector: "Translation services",
+        },
+        {
+            value: "21",
+            sector: "Transport and Logistics",
+        },
+        {
+            value: "111",
+            sector: "Air",
+        },
+        {
+            value: "114",
+            sector: "Rail",
+        },
+        {
+            value: "112",
+            sector: "Road",
+        },
+        {
+            value: "113",
+            sector: "Water",
+        },
+    ]);
+}
